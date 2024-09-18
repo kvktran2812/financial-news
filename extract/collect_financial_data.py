c11e6c2c37454efc958e59c7cef3b07e43262066
kvktran2812 <kvktran@gmail.com>
2024-09-17 21:09:14 -0400

    add get ticker data

--- extract/collect_financial_data.py
+++ extract/collect_financial_data.py
@@ -2,15 +2,6 @@ import yfinance as yf
 import pandas as pd
 
 
-
-ticker = "TSLA"
-
-stock_data = yf.download(tickers=ticker, period="max", interval="1d")
-
-print(stock_data.head(10))
-print(len(stock_data))
-
-
 def get_financial_data(ticker, period="max", interval="1d"):
     data = yf.download(tickers=ticker, period=period, interval=interval)
     return data
--- extract/collect_tickers.py
+++ extract/collect_tickers.py
@@ -1,16 +1,16 @@
-import requests
 import bs4
-from typing import List, Sequence, Dict, Tuple
-
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from typing import List, Sequence, Dict, Tuple
 
 
-def get_all_tickers(web_driver: str) -> List[str]:
+def get_all_tickers() -> List[str]:
     url = "https://stockanalysis.com/stocks/"
 
     # Set chrome options
@@ -18,13 +18,10 @@ def get_all_tickers(web_driver: str) -> List[str]:
     chrome_options.add_argument("--headless")
     
     driver = webdriver.Chrome(options=chrome_options)
-
     driver.get(url)
 
-    driver.get(url)
-    time.sleep(1)
-
-    elements = driver.find_elements(By.TAG_NAME, 'h1')
+    wait = WebDriverWait(driver, 10)
+    elements = wait.until(EC.presence_of_all_elements_located((By.ID, 'main')))
 
     for e in elements:
         print(e.text)
@@ -33,4 +30,31 @@ def get_all_tickers(web_driver: str) -> List[str]:
     return
 
 
-get_all_tickers("../web_driver/chromedriver.exe")
+def get_table_head_data(table: str) -> List[str]:
+    thead_data = []
+    thead = table.find('thead')
+    ths = thead.find_all("th")
+
+    for th in ths:
+        thead_data.append(th.text.strip())
+
+    return thead_data
+
+
+def get_table_body_data(table, n: int = -1) -> List[List[str]]:
+    tbody_data = []
+    tbody = table.find("tbody")
+    trs = tbody.find_all('tr')
+
+    for tr in trs:
+        td = tr.find_all('td')
+        td = [t.text.strip() for t in td]
+        tbody_data.append(td)
+    return tbody_data
+
+
+def get_ticker_detail() -> Dict[str, str]:
+    return
+
+
+get_all_tickers()
