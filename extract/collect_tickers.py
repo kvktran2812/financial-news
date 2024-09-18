7deccd7c9a468a3a2c116e8eb41d9f6ce7c8ab0f
kvktran2812 <kvktran@gmail.com>
2024-09-18 13:34:53 -0400

    complete get all tickers data

--- extract/collect_tickers.py
+++ extract/collect_tickers.py
@@ -1,5 +1,4 @@
-import bs4
-import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -8,26 +7,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from typing import List, Sequence, Dict, Tuple
+import time
+import math
 
 
-def get_all_tickers() -> List[str]:
-    url = "https://stockanalysis.com/stocks/"
+def get_number_of_tickers(html: str) -> int:
+    n = 0
 
-    # Set chrome options
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url)
+    h2 = html.find("h2")
+    text = h2.text.split(' ')
+    n = int(text[0])
 
-    wait = WebDriverWait(driver, 10)
-    elements = wait.until(EC.presence_of_all_elements_located((By.ID, 'main')))
-
-    for e in elements:
-        print(e.text)
-
-    driver.quit()
-    return
+    return n
 
 
 def get_table_head_data(table: str) -> List[str]:
@@ -51,6 +42,44 @@ def get_table_body_data(table, n: int = -1) -> List[List[str]]:
         td = [t.text.strip() for t in td]
         tbody_data.append(td)
     return tbody_data
+
+
+def get_all_tickers(t_per_page=500) -> List[List[str]]:
+    url = "https://stockanalysis.com/stocks/"
+    data = None
+    
+    # setup chrome
+    chrome_options = Options()
+    driver = webdriver.Chrome(options=chrome_options)
+    wait = WebDriverWait(driver, 10)
+
+    # setup elements
+    driver.get(url)
+    elements = wait.until(EC.presence_of_element_located((By.ID, 'main')))
+    next_button = driver.find_element(By.XPATH, '//*[@id="main"]/div/div/nav/button[2]')
+
+    # fetch data
+    html = elements.get_attribute("innerHTML")
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find("table", {"id": "main-table"})
+    data = get_table_body_data(table)
+    n = get_number_of_tickers(soup)
+    n = n // 500
+    
+
+    for i in range(n):
+        next_button.click()
+        time.sleep(0.5)
+        elements = wait.until(EC.presence_of_element_located((By.ID, 'main')))
+        html = elements.get_attribute("innerHTML")
+        soup = BeautifulSoup(html, 'lxml')
+        table = soup.find("table", {"id": "main-table"})
+        t_data = get_table_body_data(table)
+        data.extend(t_data)
+
+    driver.quit()
+    
+    return data
 
 
 def get_ticker_detail() -> Dict[str, str]:
