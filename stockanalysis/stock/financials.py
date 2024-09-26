f079c859bed7d23be1886174ed821250288f8fa7
kvktran2812 <kvktran@gmail.com>
2024-09-26 13:57:41 -0400

    add compute url

--- stockanalysis/stock/financials.py
+++ stockanalysis/stock/financials.py
@@ -12,7 +12,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 # define constants
-BASE_URL = "https://stockanalysis.com/stocks/"
+BASE_URL = "https://stockanalysis.com/stocks"
+INCOME = "financials"
 BALANCE_SHEET = "balance-sheet"
 CASH_FLOW = "cash-flow-statement"
 RATIOS = "ratios"
@@ -22,6 +23,21 @@ FM_ANNUAL = 1
 FM_QUARTERLY = 2
 FM_TTM = 3
 
+
+def get_fiscal_period_params(fiscal_period):
+    if fiscal_period == FM_ANNUAL:
+        return ""
+    elif fiscal_period == FM_QUARTERLY:
+        return "quarterly"
+    elif fiscal_period == FM_TTM:
+        return "trailing"
+    else:
+        raise ValueError(f"Invalid fiscal period: {fiscal_period}")
+    
+
+def compute_url(stock, fiscal_period, financial_type):
+    pass
+    
 
 def get_number_of_columns(rows):
     first_row = rows[0]
@@ -52,8 +68,12 @@ def get_data_from_tbody(rows, data, n):
             data[i][attribute] = t_data[i+1].text.strip()
 
 
-def get_income_statement(stock: str):
-    url = "https://stockanalysis.com/stocks/aapl/financials/"
+def get_income_statement(stock: str, fiscal_period: str = FM_ANNUAL) -> List:
+    FP = get_fiscal_period_params(fiscal_period)
+    url = f"{BASE_URL}/{stock}/{INCOME}/?p={FP}"
+    print(url)
+
+
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html, "lxml")
@@ -89,3 +109,8 @@ def get_ratios(stock: str, interval: str = "annually") -> List:
     
     
     return
+
+
+
+if __name__ == "__main__":
+    print(get_income_statement("aapl"))
