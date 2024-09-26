b810fc26a7986ec35b3e8a452bb8d45581a4268f
kvktran2812 <kvktran@gmail.com>
2024-09-26 14:25:52 -0400

    complete financials fetch

--- stockanalysis/stock/financials.py
+++ stockanalysis/stock/financials.py
@@ -19,9 +19,9 @@ CASH_FLOW = "cash-flow-statement"
 RATIOS = "ratios"
 
 # fiscal period type
-FM_ANNUAL = 1
-FM_QUARTERLY = 2
-FM_TTM = 3
+FM_ANNUAL = "annual"
+FM_QUARTERLY = "quarter"
+FM_TTM = "ttm"
 
 
 def get_fiscal_period_params(fiscal_period):
@@ -35,8 +35,25 @@ def get_fiscal_period_params(fiscal_period):
         raise ValueError(f"Invalid fiscal period: {fiscal_period}")
     
 
+def get_statement_type(statement_type):
+    if statement_type == "income":
+        return INCOME
+    elif statement_type == "balance":
+        return INCOME + "/" + BALANCE_SHEET
+    elif statement_type == "cash":
+        return INCOME + "/" + CASH_FLOW
+    elif statement_type == "ratios":
+        return INCOME + "/" + RATIOS
+    else:
+        raise ValueError(f"Invalid statement type: {statement_type}")
+
+    
+
 def compute_url(stock, fiscal_period, financial_type):
-    pass
+    fiscal_period = get_fiscal_period_params(fiscal_period)
+    statement_type = get_statement_type(financial_type)
+    url = f"{BASE_URL}/{stock}/{statement_type}/?p={fiscal_period}"
+    return url
     
 
 def get_number_of_columns(rows):
@@ -68,11 +85,9 @@ def get_data_from_tbody(rows, data, n):
             data[i][attribute] = t_data[i+1].text.strip()
 
 
-def get_income_statement(stock: str, fiscal_period: str = FM_ANNUAL) -> List:
-    FP = get_fiscal_period_params(fiscal_period)
-    url = f"{BASE_URL}/{stock}/{INCOME}/?p={FP}"
+def get_income_statement(stock: str, fiscal_period: str = "annual") -> List:
+    url = compute_url(stock, fiscal_period, "income")
     print(url)
-
 
     response = requests.get(url)
     html = response.text
@@ -93,24 +108,70 @@ def get_income_statement(stock: str, fiscal_period: str = FM_ANNUAL) -> List:
     return data
 
 
-def get_balance_sheet(stock: str, fiscal_period: str = "annually") -> List:
+def get_balance_sheet(stock: str, fiscal_period: str = "annual") -> List:
+    url = compute_url(stock, fiscal_period, "balance")
+    print(url)
+
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "lxml")
+    table = soup.find("table", {"id": "main-table"})
+    thead = table.find("thead")
+    tbody = table.find("tbody")
+    head_rows = thead.find_all("tr")
+    body_rows = tbody.find_all("tr")
     
+    # Data initialization
+    n = get_number_of_columns(head_rows)
+    data = [{} for _ in range(n)]
+
+    get_data_from_thead(head_rows, data, n)
+    get_data_from_tbody(body_rows, data, n)
     
-    return
+    return data
 
 
-def get_cash_flow(stock: str, fiscal_period: str = "annually") -> List:
+def get_cash_flow(stock: str, fiscal_period: str = "annual") -> List:
+    url = compute_url(stock, fiscal_period, "cash")
+    print(url)
+
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "lxml")
+    table = soup.find("table", {"id": "main-table"})
+    thead = table.find("thead")
+    tbody = table.find("tbody")
+    head_rows = thead.find_all("tr")
+    body_rows = tbody.find_all("tr")
     
+    # Data initialization
+    n = get_number_of_columns(head_rows)
+    data = [{} for _ in range(n)]
+
+    get_data_from_thead(head_rows, data, n)
+    get_data_from_tbody(body_rows, data, n)
     
-    return
+    return data
 
 
-def get_ratios(stock: str, interval: str = "annually") -> List:
+def get_ratios(stock: str, fiscal_period: str = "annual") -> List:
+    url = compute_url(stock, fiscal_period, "ratios")
+    print(url)
+
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "lxml")
+    table = soup.find("table", {"id": "main-table"})
+    thead = table.find("thead")
+    tbody = table.find("tbody")
+    head_rows = thead.find_all("tr")
+    body_rows = tbody.find_all("tr")
     
+    # Data initialization
+    n = get_number_of_columns(head_rows)
+    data = [{} for _ in range(n)]
+
+    get_data_from_thead(head_rows, data, n)
+    get_data_from_tbody(body_rows, data, n)
     
-    return
-
-
-
-if __name__ == "__main__":
-    print(get_income_statement("aapl"))
+    return data
