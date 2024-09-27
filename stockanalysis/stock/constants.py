6cc319ddf074facec2dca69937ae40da9d32e4fd
kvktran2812 <kvktran@gmail.com>
2024-09-26 23:54:17 -0400

    add utils for table scraping

--- stockanalysis/stock/constants.py
+++ stockanalysis/stock/constants.py
@@ -1,10 +1,16 @@
 BASE_URL = "https://stockanalysis.com/stocks"
 
 # other relative routes
-INCOME = "financials"
+FINANCIALS = "financials"
 STATISTICS = "statistics"
 DIVIDEND = "dividend"
-
-# from statistics
 MARKET_CAP = "market-cap"
 REVENUE = "revenue"
+FORECAST = "forecast"
+PROFILE = "company"
+EMPLOYEES = "employees"
+
+# routes related to financials
+BALANCE_SHEET = "balance-sheet"
+CASH_FLOW = "cash-flow-statement"
+RATIOS = "ratios"
