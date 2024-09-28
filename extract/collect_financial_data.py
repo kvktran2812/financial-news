990135f3c7ae2bee7543de517c14b3165b9ab5d7
kvktran2812 <kvktran@gmail.com>
2024-09-28 16:50:29 -0400

    restructure project

--- extract/collect_financial_data.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-
-def get_financial_data(ticker, period="max", interval="1d"):
-    data = yf.download(tickers=ticker, period=period, interval=interval)
-    return data
